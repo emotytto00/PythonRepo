2fa9c4a0f66a9afa7ed37694decce3ab75a7a566
LAPTOP-6COMPGRC\Johannes Liikanen <johannes.liikanen@gmail.com>
2022-08-24 11:28:51 +0300

    repon järjestelyä

--- Moduuli2/tehtävä2.py
+++ Moduuli2/tehtävä2.py
@@ -1,11 +1,13 @@
-print('Hello Johannes')
+#print('Hello Johannes')
 #print('Eka', end='ö')
 #print('Toka')
-print('Tämä ohjelma muuntaa kilokalorit jouleksi')
+#print('Tämä ohjelma muuntaa kilokalorit jouleksi')
+import math
 
-kaloritMJ = input('Anna energiasisltö kilokaloreina:')
-print('Muunnetaan ' + kaloritMJ + ' kilokaloria kilojouleiksi')
-kalorit = float(kaloritMJ)
+#print('Muunnetaan ' + kaloritMJ + ' kilokaloria kilojouleiksi')
+#kalorit = float(kaloritMJ)
 #kilojoulet = 4.184 * kalorit
 
-print(f'Se on {kaloritMJ*4.184:.0f} kilojoulea.')
+#print(f'Se on {kaloritMJ*4.184:.0f} kilojoulea.')
+säde = input('Mikä on ympyrän säde?')
+print(f'Pinta-ala on {float(math.pi*math.pi)/säde} ')
