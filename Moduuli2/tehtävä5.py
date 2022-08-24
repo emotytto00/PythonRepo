740e0c9920ed04945212c6d745817a873f333e78
LAPTOP-6COMPGRC\Johannes Liikanen <johannes.liikanen@gmail.com>
2022-08-24 22:46:27 +0300

    tehtävien tekoa. tehtävä 5 kesken

---
+++ Moduuli2/tehtävä5.py
@@ -0,0 +1,10 @@
+leiviskä = float(input('Anna leiviskät'))
+naula = float(input('Anna naulat'))
+luoti = float(input('Anna luodit'))
+
+luotiM = float(13.3 * luoti)
+naulaM = float(32 * luoti)
+leiviskäM = float(20 * naula)
+
+print(f'Massa nykymitoissa {(luotiM + naulaM + leiviskäM)}')
+
