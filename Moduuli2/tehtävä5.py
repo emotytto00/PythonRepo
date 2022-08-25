a84086e1c8c423bc4819a3ba0a3663d1b05e7a26
LAPTOP-6COMPGRC\Johannes Liikanen <johannes.liikanen@gmail.com>
2022-08-25 11:20:52 +0300

    tehtävien tekoa. tehtävä 5 kesken

--- Moduuli2/tehtävä5.py
+++ Moduuli2/tehtävä5.py
@@ -5,6 +5,12 @@ luoti = float(input('Anna luodit'))
 luotiM = float(13.3 * luoti)
 naulaM = float(32 * luoti)
 leiviskäM = float(20 * naula)
+massa = (luotiM + naulaM + leiviskäM)
 
-print(f'Massa nykymitoissa {(luotiM + naulaM + leiviskäM)}')
+if(massa < 1000 ):
+    print(f'{massa:0.4f} g')
+
+
+print(f'Massa nykymitoissa: {massa:0.0f} kg {massa:0.4f}')
+
 
