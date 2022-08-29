5be848c31290ce549acd35f1793a76c8d6db311f
emotytto00 <johannes.liikanen@gmail.com>
2022-08-29 11:09:42 +0300

    teht 4 korjattu

--- Moduuli3/tehtävä4.py
+++ Moduuli3/tehtävä4.py
@@ -5,6 +5,7 @@ tulos = 'ei ole'
 if vuosi % 4 == 0:
     if vuosi % 100 != 0:
         tulos = 'on'
-    if vuosi % 400 != 0:
+    if vuosi % 400 == 0:
         tulos = 'on'
+
 print(f'vuosi {vuosi} {tulos} karkausvuosi')
