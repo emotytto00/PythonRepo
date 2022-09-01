fc4267bad6ced58eba46886a15ed4527cd0219f9
emotytto00 <johannes.liikanen@gmail.com>
2022-09-01 13:26:01 +0300

    tehtävä 5 suoritettu

---
+++ Moduuli4/tehtävä5.py
@@ -0,0 +1,26 @@
+toistot = 0
+käyttäjä = 'python'
+salasana = 'rules'
+
+while toistot <= 5:
+    käy = input('Syötä käyttäjätunnus: ')
+    sal = input('Syötä salasana: ')
+    if käy == käyttäjä:
+        if sal == salasana:
+            print('Teretulemast tallinkil')
+            break
+        else:
+            print('Pääsy evätty')
+            toistot = toistot +1
+    else:
+        print('Pääsy evätty')
+        toistot = toistot +1
+
+        #vaihtoehtoinen ratkaisu laitettu kommentteihin
+
+#while (käy!=str('python') and sal!=str('rules)'
+    #if käy!=str('python') or sal!=str('Rules')
+      #  print('Pääsy evätty')
+    #käy = input('Anna käyttis')
+    #sal = input('Anna salis')
+    #print('Tervetuloa)'
