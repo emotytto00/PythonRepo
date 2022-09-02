59d51ffbfc34d2f1d1bf1a411193646cee5b7b26
emotytto00 <johannes.liikanen@gmail.com>
2022-09-02 10:42:52 +0300

    pientä pintaremonttia

--- Moduuli4/tehtävä3.py
+++ Moduuli4/tehtävä3.py
@@ -9,3 +9,14 @@ except ValueError:
     lista.sort()
     print('Suurin numero oli', max(lista), 'Pienin numero oli', min(lista))
     sys.exit(0)
+
+#vaihtoehtoinen ratkaisu
+#lukustr = input('Anna luku tai lopeta tyhjällä syötteellä: ')
+#while lukustr !="":
+#   lukustr = input('Anna seuraava luku tai lopeta tyhjällä: ')
+#       luku = float(lukustr)
+#           if luku < pienin:
+                #pienin = luku
+            #if luku> suurin:
+            #   suurin = luku
+#print(f'Antamistasi luvuista pienin oli {pienin} ja suurin oli {suurin}')
