3a8ee1833a2a6ff27b4d3f1d798ba6a2c73a7233
emotytto00 <johannes.liikanen@gmail.com>
2022-09-08 20:15:50 +0300

    tehtävä 5 tehty.

--- Moduuli6/tehtävä5.py
+++ Moduuli6/tehtävä5.py
@@ -1,6 +1,10 @@
 def luvut(numb):
     lista = []
     for t in numb:
-        summa = summa + t
-    return summa
-lista = [4, 20, 6, 9]
+        if t % 2 == 0:
+            lista.append(t)
+    return lista
+
+lista2 = [1,2,3,4,5,6,7,8,9,10]
+print(lista2)
+print(luvut(lista2))
