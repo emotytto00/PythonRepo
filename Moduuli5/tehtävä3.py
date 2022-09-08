1dd6e29c233d9198b00a13339fd8bfba93665413
emotytto00 <johannes.liikanen@gmail.com>
2022-09-08 18:01:21 +0300

    tehtävä3 tehty.

--- Moduuli5/tehtävä3.py
+++ Moduuli5/tehtävä3.py
@@ -1,10 +1,13 @@
 numero = int(input('Anna luku: '))
-
-alkuluku = True
-
-for luku in range(2, numero, 1):
-
-if alkuluku == True:
-    print('On alkuluku')
+jako = False
+if numero != 2 and not numero % 2:
+    jako = True
+for i in range(2,numero):
+    if not numero % i:
+        jako = True
+    if jako:
+        break
+if jako:
+    print(f'{numero} ei ole alkuluku')
 else:
-    print('Nou madame')
+    print(f'{numero} on alkuluku')
