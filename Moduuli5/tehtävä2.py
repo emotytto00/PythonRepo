b3b3ebda3cd515e4e091d5050ba473d9c79a8a15
emotytto00 <johannes.liikanen@gmail.com>
2022-09-08 17:42:28 +0300

    tehtävä2 tehty

--- Moduuli5/tehtävä2.py
+++ Moduuli5/tehtävä2.py
@@ -4,6 +4,6 @@ numero = input('Anna ensimmäinen luku tai syötä tyhjä')
 while numero != "":
     numerot.append(numero)
     numero = input('Anna toinen luku tai syötä tyhjä')
-
-
-print(numerot)
+    numerot.sort(reverse=True)
+for i in range(1):
+    print(numerot[0:5])
