687e9002cfcb93a351063dcc29e929009d8afdc2
emotytto00 <johannes.liikanen@gmail.com>
2022-09-14 15:41:23 +0300

    tehtävä 3 tehty

--- Moduuli7/tehtävä3.py
+++ Moduuli7/tehtävä3.py
@@ -1,13 +1,24 @@
 lentoasemat = {
-    'EFHK' : 'Helsinki-Vantaan Lentoasema', '42OK' : 'Kingston international airport',
+    'EFHK' : 'Helsinki-Vantaan Lentoasema', 'HHOK' : 'Kingston international airport',
 }
 hakusana = [lentoasemat]
 teko = str
 while teko != '':
-    teko = input('Syötä uusi, hae entistä tai lopeta: ')
-    if teko in lentoasemat:
-        print(lentoasemat[hakusana])
-    else:
+    teko = input('Syötä uusi (uusi) '
+                 'Hae entistä (hae) '
+                 'Lopeta syöttämällä tyhjä: ')
+    if teko.upper() == "UUSI":
+        nimi = input('Anna lentoaseman nimi: ')
+        lentoasemat[input('Anna aseman ICAO koodi: ')] = nimi
+
+    if teko.upper() == "HAE":
+        ICAO = input('Syötä aseman ICAO koodi: ')
+        print(f'koodilla {ICAO} löytyi lentokenttä: {lentoasemat[ICAO]}')
+else:
+    print('Ohjelma lopetetaan. Prepare for selfdestruction in 5,4,3,2,1... BOOOOMM!!!!!')
+
+
+
 
 lentoasemat['AGRG'] = 'Ulawa Airport'
 lentoasemat['SJRM'] = 'Seinäjoen tango airport'
